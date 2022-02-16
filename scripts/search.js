aef4b9db0213d3a804eff7b495b9db0af739fa1e
BRAT96 <dan.hurenko@gmail.com>
2022-02-17 02:26:05 +0300

    15commit

--- scripts/search.js
+++ scripts/search.js
@@ -1,16 +1,29 @@
 var btn = document.getElementById('btnSearch');
 
+function sleep(ms) {
+ms += new Date().getTime();
+while (new Date() < ms){}
+}
+
   function Search()
   {
       const collection = document.getElementsByClassName("god__name");
       var whatSearch = document.getElementById("what_search").value;
       for(i=0; i<collection.length; i++)
       {
+        collection[i].parentNode.parentNode.classList.remove("god1");
         if(collection[i].textContent.toLowerCase() == whatSearch || collection[i].textContent == whatSearch)
         {
           collection[i].parentNode.scrollIntoView({block: "center", behavior: "smooth"});
+          collection[i].parentNode.parentNode.classList.add("god1");
         }
       }
   }
+
+  document.querySelector("#what_search").addEventListener("keyup", event => {
+      if(event.key !== "Enter") return;
+      document.querySelector("#btnSearch").click();
+      event.preventDefault();
+  });
 
 btn.addEventListener("click", Search);
